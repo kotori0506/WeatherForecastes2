4bbe5f12d810c1138837d9623c98cbc170e66f49
kotori0506 <kotori0506studio@yahoo.co.jp>
2015-10-08 20:00:15 +0900

    update 151008 20:00

--- WeatherForecasts2/app/src/main/java/com/example/ktr/weatherforecasts2/MyActivity.java
+++ WeatherForecasts2/app/src/main/java/com/example/ktr/weatherforecasts2/MyActivity.java
@@ -1,18 +1,53 @@
 package com.example.ktr.weatherforecasts2;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import android.os.Handler;
 
 
 public class MyActivity extends ActionBarActivity {
 
+    private TextView textView;
+    private Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        textView=(TextView)findViewById(R.id.tv_main);
+
+        Thread thread = new Thread(){
+            @Override
+        public void run(){
+             try {
+                 final String data = WeatherApi.getWeather(MyActivity.this, "400040");
+
+                 handler.post(new Runnable(){
+                                 @Override
+                                public void run(){
+                                     textView.setText(data);
+                                 }
+                              });
+             }catch (final IOException e){
+                 handler.post(new Runnable(){
+                     @Override
+                    public void run(){
+                         Toast.makeText(MyActivity.this,e.getMessage(),
+                                 Toast.LENGTH_SHORT).show();
+                     }
+                 });
+             }
+            }
+        };
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
